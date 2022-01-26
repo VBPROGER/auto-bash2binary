7656caaa52941d424f9dd7a3ac35287838dd8c04
VB PROGER <97066898+VBPROGER@users.noreply.github.com>
2022-01-26 11:53:57 +0300

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+clear || cls || : > /dev/null
+
+NAME="auto-bash2binary"
+VERSION="1.0.0"
+
+LOG(){
+printf "$NAME v$VERSION: $1\n"
+}
+build(){
+LOG "Building \"$1\"..."
+shc -f $1 > /dev/null
+LOG "Moving..."
+mv "$1.x" "$1.bin"
+rm "$1.x.c"
+LOG "Finished building \"$1\""
+}
+
+if [ "$1" != "" ]; then
+    LOG "Starting..."
+    build "$1"
+else
+    LOG "ERR: File name is not given"
+    exit
+fi
